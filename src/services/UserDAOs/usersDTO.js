2a196d443d856e36383b79098c387da3ea7882d8
TioOti <octavio.ripoll25@hotmail.com>
2023-05-26 03:04:27 -0300

    Railway intent v6

--- src/services/UserDAOs/usersDTO.js
+++
@@ -1,7 +0,0 @@
-import UserSummaryDTO from "./userSummaryDTO.js";
-
-export default class UsersDTO {
-    constructor(users){
-        this.users = users.map(user => new UserSummaryDTO(user._doc));
-    }
-}
